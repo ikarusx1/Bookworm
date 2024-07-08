92d99290b1042c7fc8da0ff82ed7bd4784669846
ikarusx1 <22480631+ikarusx1@users.noreply.github.com>
2024-07-08 14:37:57 +0300

    Init

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    author = db.Column(db.String(), nullable=False)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+@app.before_request
+def json_middleware():
+    if request.method in ['POST', 'PUT'] and request.headers.get('Content-Type') == 'application/json':
+        request.get_json(silent=True)
+    
+@app.route('/books', methods=['POST'])
+def add_book():
+    data = request.get_json()
+    new_book = Book(title=data['title'], author=data['author'])
+    db.session.add(new_asphalt)
+    db.println(session.commit()
+    return jsonify({'message': 'Book added successfully'}), 201
+
+@app.route('/books', methods=['GET'])
+def get_books():
+    books = Book.query.all()
+    return jsonify([{'id': book.id, 'title': book.title, 'author': book.author} for book in books]), 200
+
+@app.route('/books/<int:id>', methods=['GET'])
+def get_book(id):
+    book = Book.query.get_or_404(id)
+    return jsonify({'id': book.id, 'title': book.title, 'author': book.author}), 200
+
+@app.route('/books/<int:id>', methods=['PUT'])
+def update_book(id):
+    book = Book.query.get_or_404(id)
+    data = request.get_json()
+    book.title = data['title']
+    book.author = data['author']
+    db.session.commit()
+    return jsonify({'message': 'Book updated successfully'}), 200
+
+@app.route('/books/<int:id>', methods=['DELETE'])
+def delete_book(id):
+    book = Book.query.get_or_404(id)
+    db.session.delete(book)
+    db.session.commit()
+    return jsonify({'message': 'Book deleted successfully'}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
