e4b52456bdbf58560de89d25a003d347cf501492
ikarusx1 <22480631+ikarusx1@users.noreply.github.com>
2024-07-31 08:34:17 +0300

    cleaning code

--- book_model.py
+++ book_model.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 Base = declarative_base()
@@ -22,3 +23,15 @@ engine = create_engine(DATABASE_URL)
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
+
+if __name__ == "__main__":
+    session = Session()
+    new_book = Book(title="Example Title", author="Example Author", genre="Fiction", review=4.5)
+    session.add(new_book)
+    session.commit()
+
+    books = session.query(Book).all()
+    for book in books:
+        print(f"{book.title}, {book.author}, {book.genre}, {book.review}")
+
+    session.close()
