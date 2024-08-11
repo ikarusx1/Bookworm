99b9db5e36b548c79a87fccda4ee1a6b6277011a
ikarusx1 <22480631+ikarusx1@users.noreply.github.com>
2024-08-11 09:50:48 +0300

    improve variables

--- db.py
+++ db.py
@@ -2,13 +2,25 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
+
+# Load environment variables
 load_dotenv()
-db_name = os.getenv("DATABASE_NAME")
-db_user = os.getenv("DATABASE_USER")
-db_pass = os.getenv("DATABASE_PASSWORD")
-db_host = os.getenv("DATABASE_HOST", "localhost")
-db_port = os.getenv("DATABASE_PORT", "5432")
-db_connection_url = f"postgresql+psycopg2://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
-engine = create_engine(db_connection_url)
-Session = sessionmaker(bind=engine)
-session = Session()
+
+# Database Configuration Variables
+database_name = os.getenv("DATABASE_NAME")
+database_user = os.getenv("DATABASE_USER")
+database_password = os.getenv("DATABASE_PASSWORD")
+database_host = os.getenv("DATABASE_HOST", "localhost")
+database_port = os.getenv("DATABASE_PORT", "5432")
+
+# Create Database Connection URL
+database_connection_url = f"postgresql+psycopg2://{database_user}:{database_password}@{database_host}:{database_port}/{database_name}"
+
+# Initialize Database Engine
+database_engine = create_engine(database_connection_url)
+
+# Create a SessionMaker instance
+DatabaseSession = sessionmaker(bind=database_engine)
+
+# Initialize a Session
+database_session = DatabaseSession()
